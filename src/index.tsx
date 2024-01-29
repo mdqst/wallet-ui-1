0f9f6818f91f6707624e59ded81b880fc5184ec0
bhumitaylor14 <141008551+bhumitaylor14@users.noreply.github.com>
2024-01-29 15:38:09 +0530

    remove unnecessary css

--- src/index.tsx
+++ src/index.tsx
@@ -29,6 +29,7 @@ import './index.css';
 
 // local
 import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
